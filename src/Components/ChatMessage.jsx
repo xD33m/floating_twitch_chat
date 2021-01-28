6d29e53450e18f387f46c4e57765c42f6a57ffd9
Lucas <lucaskeller98@gmail.com>
2021-01-28 17:02:01 +0100

    basic

---
+++ src/Components/ChatMessage.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+
+class ChatMessage extends Component {
+	render() {
+		const { username, badges, message, color } = this.props;
+		return (
+			<div className="chat-line visible">
+				<div className="chat-line-inner">
+					<span className="badges">
+						{badges.map((badge) => {
+							return (
+								<img
+									className="badge"
+									src={badge.url}
+									badgetype={badge.type}
+									alt={badge.type}
+								/>
+							);
+						})}
+					</span>
+					<span className="username" style={{ color: color }}>
+						{username}
+					</span>
+					<span className="message-colon">: </span>
+					<span className="message">
+						{message.map((msg) => {
+							if (typeof msg === 'string') {
+								return msg;
+							} else {
+								return <img src={msg.url} alt={msg.alt} />;
+							}
+						})}
+					</span>
+				</div>
+			</div>
+		);
+	}
+}
+
+export default ChatMessage;
