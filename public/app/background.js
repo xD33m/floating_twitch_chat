c855754bcff9fa5b38177abdca87bbacfb931d91
Lucas <lucaskeller98@gmail.com>
2023-06-21 19:24:26 +0200

    remove tabs permission, it's not needed

--- public/app/background.js
+++ public/app/background.js
@@ -1,13 +1,4 @@
 /*global chrome*/
-// Called when the user clicks on the browser action
-chrome.browserAction.onClicked.addListener(function (tab) {
-	// Send a message to the active tab
-	chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-		var activeTab = tabs[0];
-		chrome.tabs.sendMessage(activeTab.id, { message: 'clicked_browser_action' });
-	});
-});
-
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
 	if (message === 'getScreenState') {
 		chrome.windows.get(sender.tab.windowId, function (chromeWindow) {
